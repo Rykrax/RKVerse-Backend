dbae07b683927960264ee1532d09da2e7df5c503
Rykrax <rykrax.ksnd.1122@gmail.com>
2026-08-25 01:35:36 +0700

    feat: init project

---
+++ src/main/java/dev/rykrax/rkverse/RkVerseApplication.java
@@ -0,0 +1,13 @@
+package dev.rykrax.rkverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkVerseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RkVerseApplication.class, args);
+    }
+
+}
