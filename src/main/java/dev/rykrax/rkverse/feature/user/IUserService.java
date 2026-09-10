6582856e385653586a54581700eab90f38f1d9bc
Phạm Tuấn Khang <94215800+Rykrax@users.noreply.github.com>
2026-09-10 23:34:44 +0700

    feat: add pagination for user (#6)

--- src/main/java/dev/rykrax/rkverse/feature/user/IUserService.java
+++ src/main/java/dev/rykrax/rkverse/feature/user/IUserService.java
@@ -1,13 +1,15 @@
 package dev.rykrax.rkverse.feature.user;
 
+import dev.rykrax.rkverse.common.PageResponse;
 import dev.rykrax.rkverse.feature.user.dto.request.ChangePasswordRequest;
 import dev.rykrax.rkverse.feature.user.dto.response.UserResponse;
 import dev.rykrax.rkverse.security.CustomUserDetail;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface IUserService {
-    List<UserResponse> getUsers();
+    PageResponse<UserResponse> getUsers(Pageable pageable);
     UserResponse getUserById(Long id);
     UserResponse getMe(CustomUserDetail userDetails);
     UserResponse update(Long id);
